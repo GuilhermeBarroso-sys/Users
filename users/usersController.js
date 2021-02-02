01278e285cf58f122c18fbf70c87924b94ea781e
GuilhermeBarroso-Sys <golfinhoninja@hotmail.com>
2021-02-02 17:47:21 -0300

    teste

--- users/usersController.js
+++ users/usersController.js
@@ -5,7 +5,7 @@ router.get("/login",(req,res) => {
     res.render("login/signUp");
 })
 router.get("/signIn",(req,res) => {
-    res.render("login/signIn");
+    res.render("login/signIn"); // cadastrar
 })
 
 module.exports = router;
